2fc57afbe5798f55a7f9dddd108612dacd9b7d8e
rammpk <rammpk@gmail.com>
2025-07-14 17:05:09 +0000

    inciando mudancas de layout

--- menu.js
+++ menu.js
@@ -1,51 +1,59 @@
-const icons = [
+const iconsData = [
     {
         menuIcon: "wix:vector://v1/b98454_e2d61ef8c55a457b935cc91733e7ed36.svg/icone-categoria-revestimentos.svg",
         menuTitle: "Revestimentos",
         pageUrl: "/conteudo-categorias/revestimentos",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Revestimentos."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Revestimentos.",
+        catalogoUrl: "revestimentos"
     },
     {
         menuIcon: "wix:vector://v1/b98454_e4d327dd54044fce87525229fd82a6f5.svg/icone-categoria-material-bruto.svg",
         menuTitle: "Materiais Brutos",
         pageUrl: "/conteudo-categorias/materiais-brutos",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Materiais%20Brutos."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Materiais%20Brutos.",
+        catalogoUrl: "materiais"
     },
     {
         menuIcon: "wix:vector://v1/b98454_4be528f0a66c4f128e36bcef8dc54a10.svg/icone-categoria-pintura.svg",
         menuTitle: "Pintura",
         pageUrl: "/conteudo-categorias/pintura",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Pintura."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Pintura.",
+        catalogoUrl: ""
     },
     {
         menuIcon: "wix:vector://v1/b98454_e7d01e6236564ebd8d78f3b5c74328a1.svg/icone-categoria-ferramentas.svg",
         menuTitle: "Ferramentas",
         pageUrl: "/conteudo-categorias/ferramentas",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Ferramentas."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Ferramentas.",
+        catalogoUrl: ""
     },
     {
         menuIcon: "wix:vector://v1/b98454_d2b663ae404c4ddba2570e61ca2595ad.svg/icone-categoria-hidr%C3%A1ulica.svg",
         menuTitle: "Hidráulica",
         pageUrl: "/conteudo-categorias/hidr%C3%A1ulica",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Hidráulica."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Hidráulica.",
+        catalogoUrl: ""
     },
     {
         menuIcon: "wix:vector://v1/b98454_73f9d0cdabb047e6a4e125e61beee918.svg/icone-categoria-iluminacao-e-eletrica.svg",
         menuTitle: "Iluminação e elétrica",
         pageUrl: "/conteudo-categorias/ilumina%C3%A7%C3%A3o-e-el%C3%A9trica",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Iluminação%20e%20Elétrica."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Iluminação%20e%20Elétrica.",
+        catalogoUrl: ""
     },
     {
         menuIcon: "wix:vector://v1/b98454_f4d4365844ff4a72adf6f62054d37e27.svg/icone-categoria-casa-e-jardim.svg",
         menuTitle: "Casa e jardim",
         pageUrl: "/conteudo-categorias/casa-e-jardim",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Casa%20e%20Jardim."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Casa%20e%20Jardim.",
+        catalogoUrl: ""
     },
     {
         menuIcon: "wix:vector://v1/b98454_c1a536cb58614eefb8e6ff82d8638240.svg/icone-categoria-linha-industrial.svg",
         menuTitle: "Linha industrial",
         pageUrl: "/conteudo-categorias/linha-industrial",
-        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Linha%20Industrial."
+        url: "https://wa.me/5562982962832?text=Olá!%20Gostaria%20de%20saber%20mais%20sobre%20Linha%20Industrial.",
+        catalogoUrl: ""
     },
 ]
 
@@ -111,6 +119,7 @@ function createIconElements(icons) {
     let html = ``;
     if (icons && icons.length > 0) {
         icons.forEach(icon => {
+            if(!icon) return;
             const imageUrl = convertWixImageUrl(icon.menuIcon);
             html += `
                         <div class="icon-item-container">
@@ -160,12 +169,12 @@ function setupIcons(iconsData, urlSlug) {
     const iconItems = document.querySelectorAll('.icon-item');
     const currentPagePath = window.location.pathname;
     const currentUrl = window.location.href;
-    
+
     // Encontrar o icon baseado na URL atual
     activeIconData = iconsData.find(icon => {
         return urlSlug === getUrlSlug(icon.pageUrl);
     });
-  
+
     iconItems.forEach((item, index) => {
         const icon = iconsData.find(data => data.pageUrl === item.dataset.url);
 
@@ -268,6 +277,35 @@ function setupContainerEventListeners() {
     });
 }
 
+const aboutMaterialBtn = document.querySelector('.about-material-btn');
+const catalogueBtn = document.querySelector('.catalogue-btn');
+const infoCardsContainer = document.querySelector('.info-cards-container');
+const iconsContent = document.querySelector('.icons-content');
+
+aboutMaterialBtn.addEventListener('click', function () {
+    infoCardsContainer.classList.toggle('hide');
+    iconsContent.classList.toggle('show');
+});
+
+catalogueBtn.addEventListener('click', function () {
+    const outterIconsContent = document.querySelector('.icons-content');
+
+    if (outterIconsContent) {
+        outterIconsContent.innerHTML = '';
+    }
+
+    const iconsWithCatalogueUrl = iconsData.map(icon => {
+        if (icon.catalogoUrl) {
+            return {
+                ...icon
+            };
+        }
+        return null;
+    })
+
+    populateIconsContainer(outterIconsContent, iconsWithCatalogueUrl);
+});
+
 // Event listener para mensagens do parent (mantido para compatibilidade)
 /**     const messageData = event.data;
 
@@ -293,8 +331,8 @@ window.addEventListener('resize', handleScreenSizeChange);
 
 document.addEventListener('DOMContentLoaded', () => {
     // Para demo, populate com dados mock
-    populateContainer(icons);
-    setupIcons(icons, '/home');
+    populateContainer(iconsData);
+    setupIcons(iconsData, '/home');
     setupContainerEventListeners(); // NOVA FUNÇÃO
     handleScreenSizeChange();
 
